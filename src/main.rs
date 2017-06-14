96aa9c8e85ecaaa69b7ba49cf605f2d298693c76
Luis de Bethencourt <luis@debethencourt.com>
2017-06-14 18:14:34 +0100

    Print random C filename

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ extern crate walkdir;
 #[macro_use]
 extern crate error_chain;
 extern crate clap;
+extern crate rand;
 
 use walkdir::WalkDir;
 use std::io;
 use clap::{Arg, App};
+use rand::Rng;
 
 error_chain! {
     // Error types from other libraries that we want to just wrap
@@ -17,15 +19,24 @@ error_chain! {
 }
 
 fn run(folder: &str) -> Result<()> {
+    let mut files: Vec<String> = Vec::new();
+
     // List recusively all accessible files in the current directory
     for entry in WalkDir::new(folder).into_iter().filter_map(|e| e.ok()) {
         // Get entry's filename
         let f_name = entry.file_name().to_string_lossy();
 
-        // Print C files
+        // Add C files to collection
         if f_name.ends_with(".c") {
-            println!("{}", f_name);
+            files.push(f_name.into_owned());
         }
+    }
+
+    // Print random file from the collection
+    let mut rng = rand::thread_rng();
+    match files.get(rng.gen_range(0, files.len())) {
+        Some(x) => println!("{}", x),
+        None => (),
     }
 
     Ok(())
